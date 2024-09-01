fb850f62a7ade28c28a5a509927866f717f5b174
Brian Lim <brixnvx@gmail.com>
2024-09-01 09:08:30 +0800

    Patched data parsing

--- main.py
+++ main.py
@@ -3,21 +3,54 @@ from telegram import Update
 from telegram.ext import Application, CallbackContext, ContextTypes
 import pandas as pd
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import io
 import asyncio
+from dotenv import load_dotenv
+import os
+import gspread
+from google.oauth2.service_account import Credentials
 
-TOKEN = '6858348326:AAFpoAlVINlW08nVikkwGS8jFDV1bKgKPQM'
+# Get environment variables
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
 BOT_USERNAME: Final = '@GaruBdayBot'
-CSV_URL = "https://raw.githubusercontent.com/Garuda-Techs/GaruBdayBot/main/birthdays.csv"
-CHAT_ID = '-1002175948359'  # Replace with your actual chat ID
-TOPIC_THREAD_ID = '4' # Replace with your actual topic thread ID
+# CHAT_ID = "-1002218572340" # Testing: HC Techies Chat
+CHAT_ID = '-1002175948359'  # Production: Garuda House Chat
+TOPIC_THREAD_ID = '4' # Birthday Subtopic
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1H684nLTxJn2vQOCR1Zhv1CiqNAt535Q9yhHQCGaXqIw/edit?pli=1&gid=1262152600#gid=1262152600'
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+async def check_birthdays(application: Application):
+    today = (datetime.now() + timedelta(hours=8)).strftime('%d/%m') # Add 8 hours to work around UTC timings
+    try:
+        # Initialise and read from Google Sheets
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        credentials = Credentials.from_service_account_file("service-account-key.json", scopes=scope)
+        gc = gspread.authorize(credentials) # Authenticate and initialize the gspread client
+        spreadsheet = gc.open_by_url(SPREADSHEET_URL)
+        sheet = spreadsheet.get_worksheet(0)
+        data = sheet.get_all_values() # Get all values in the sheet
+        df = pd.DataFrame(data[1:], columns=data[0]) # Convert the data to a Pandas DataFrame
+
+        for index, row in df.iterrows():
+            if today == row['Birthday']:
+                await application.bot.send_message(
+                    chat_id=CHAT_ID,
+                    text=f"Happy Birthday {row['Name']}!",
+                    message_thread_id=TOPIC_THREAD_ID
+                )
+    
+    except Exception as e:
+        logger.error(f"Failed to fetch or process Google Sheet: {e}")
+
+"""
+CSV_URL = "https://raw.githubusercontent.com/Garuda-Techs/GaruBdayBot/main/birthdays.csv"
 async def check_birthdays(application: Application):
     today = datetime.now().strftime('%d/%m')
     try:
@@ -34,6 +67,7 @@ async def check_birthdays(application: Application):
                 )
     except Exception as e:
         logger.error(f"Failed to fetch or process CSV: {e}")
+"""
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.error(f'Update {update} caused error {context.error}')
